924273ffbac97d954769aedcbe664ee2762696fe
Gustavo Rodrigues Mietlicki <gustavo.mietlicki@deal.br>
2020-04-19 18:02:10 -0300

    Inicio do projeto

---
+++ src/main/java/com/deal/Livraria/JavaBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.deal.Livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deal/Livraria/JavaBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deal.Livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
